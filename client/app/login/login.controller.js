3bcab6c49c52958d5b359cbca769d24e45b3964e
Adeel Athar <adeel.ather@galileotechmedia.com>
2016-03-05 21:18:24 +0500

    Added bne logo

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -3,7 +3,14 @@
 angular.module('dashboardApp')
   .controller('LoginCtrl', ["$scope", "$http", "$log", "$modal", function ($scope, $http, $log, $uibModal, $modal) {
       $scope.message = 'Hello';
+
       // display signup modal
+      //$scope.signupModalInstance = $uibModal.open({
+      //    templateUrl: "signup.html",
+      //    size: "sm",
+      //    scope: $scope
+      //});
+
       $scope.toggleSignup = function (size) {
           $scope.signupModalInstance = $uibModal.open({
               templateUrl: "signup.html",
